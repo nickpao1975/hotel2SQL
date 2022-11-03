95ccc5e0f1fc7ac24d95053743432a574327df2f
Nikos Androutsopoulos <108472958+nickpao1975@users.noreply.github.com>
2022-11-03 11:34:31 +0200

    Update CRUD.sql
    transactions added from line 20

--- CRUD.sql
+++ CRUD.sql
@@ -15,3 +15,39 @@ USE hotel;
 SELECT rooms.floor, guests.name
 FROM rooms JOIN guests
 ON rooms.number = guests.idguests;
+
+
+/* a Transaction is an entity – generally a group of SQL statements, or a combination of 
+statements and stored procedure calls, which should be 
+executed all or none. With transactions operations we can commit changes/updates to a database or rollback to previous
+state.*/
+
+START transaction;
+insert INTO guests (idguests, name, rooms_number)
+values (5, "Petros Papadopoulos Ioannou", 4);
+commit;
+
+SELECT * FROM hotel.guests;
+
+START transaction;
+insert INTO guests (idguests, name, rooms_number)
+values (5, "Petros Papadopoulos Ioannou", 4);
+rollback;
+
+START transaction;
+insert INTO guests (idguests, name, rooms_number)
+values (6, "test test", 5);
+commit;
+
+SELECT * FROM hotel.guests;
+
+START transaction;
+insert INTO guests (idguests, name, rooms_number)
+values (6, "test test", 5);
+rollback;
+
+START transaction;
+insert INTO guests (idguests, name, rooms_number)
+values (7, "test test", 5);
+SELECT * FROM hotel.guests;
+commit;
